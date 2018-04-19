3e605c00257ebf0ffd892a9d855906089a5198fb
bill <billwu_@outlook.com>
2018-04-19 22:52:56 +0000

    finished login

--- express_server.js
+++ express_server.js
@@ -1,99 +1,18 @@
-var express = require('express')
-var cookieParser = require('cookie-parser')
+const express = require('express')
+const cookieParser = require('cookie-parser')
 
-var app = express()
+const app = express()
 app.use(cookieParser())
-var PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.end("Hello!");
-});
-
-var urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/urls", (req, res) => {
-  let templateVars = {
-    urls: urlDatabase,
-    username:req.cookies["username"]
-  };
-  res.render("urls_index", templateVars);
-});
-
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-
-app.get("/u/:shortURL", (req, res) => {
-  longURL = urlDatabase[req.params.shortURL]
-  console.log(longURL)
-  res.redirect(urlDatabase[req.params.shortURL]);
-});
-
-app.get("/urls/:id", (req, res) => {
-  let templateVars = {
-    shortURL: req.params.id,
-    fullURL: urlDatabase,
-    username:req.cookies["username"]
-  }
-
-  res.render("urls_show", templateVars);
-});
-
-app.post("/urls/:id", (req, res) => {
-  addHTTP(req.body.longURL, req.params.id)
-  res.redirect('/urls/');
-});
-
-app.post("/urls", (req, res) => {
-  let short = generateRandomString(6);
-  while(urlDatabase.hasOwnProperty(short) === true){
-    short = generateRandomString(6);
-    console.log(`key already exsisted. generating new key ${short}`);
-  }
-  addHTTP(req.body.longURL, short)
-  res.send(`${short} ${req.body.longURL}`);
-});
-
-app.post("/urls/:id/delete",(req, res) =>{
-  console.log(req.params.id)
-  delete urlDatabase[req.params.id];
-  res.redirect('/urls');
-});
-
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.post("/login", (req, res) =>{
-  res.cookie('username', req.body.username)
-  res.redirect('/urls')
-});
-
-app.post("/logout", (req, res) =>{
-  res.clearCookie('username', req.body.username)
-  res.redirect('/urls')
-});
-
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
-
 function generateRandomString(length) {
-  var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  for(var i = 0; i < length; i++) {
+  let text = "";
+  const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for(i = 0; i < length; i++) {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
   return text;
@@ -108,3 +27,157 @@ function addHTTP(url, shortURL){
     return urlDatabase[shortURL] = url;
   }
 }
+
+function RegenIDIfSame(Database, myGenID, value){
+  while(Database.hasOwnProperty(myGenID) === true){
+    myGenID = generateRandomString(value);
+    console.log(`key already exsisted. generating new key ${myGenID}`);
+  }
+}
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID",
+    email: "123123",
+    password: "123"
+  }
+}
+
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = {
+    urls: urlDatabase,
+    username:JSON.stringify(users[req.cookies['user_id']])
+  };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  longURL = urlDatabase[req.params.shortURL];
+  console.log(longURL);
+  res.redirect(urlDatabase[req.params.shortURL]);
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = {
+    shortURL: req.params.id,
+    fullURL: urlDatabase,
+    username:JSON.stringify(users[req.cookies['user_id']])
+  }
+
+  res.render("urls_show", templateVars);
+});
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/register", (req, res) =>{
+  res.render("urls_register");
+});
+
+app.get("/login", (req, res) => {
+  res.render("urls_login");
+});
+
+app.post("/urls/:id", (req, res) => {
+  addHTTP(req.body.longURL, req.params.id);
+  res.redirect('/urls/');
+});
+
+app.post("/urls", (req, res) => {
+  let short = generateRandomString(6);
+  RegenIDIfSame(urlDatabase, short, 6)
+  addHTTP(req.body.longURL, short);
+  res.send(`${short} ${req.body.longURL}`);
+});
+
+app.post("/urls/:id/delete",(req, res) =>{
+  console.log(req.params.id);
+  delete urlDatabase[req.params.id];
+  res.redirect('/urls');
+});
+
+
+app.post("/logout", (req, res) =>{
+  res.clearCookie('user_id', req.body.username);
+  res.redirect('/urls');
+});
+
+app.post("/register", (req, res) =>{
+  let myID = generateRandomString(6);
+  let myEmail = req.body.email;
+  let myPassword = req.body.password;
+
+  for(databaseID in users){
+    if(users[databaseID].email === myEmail){
+      res.status(400);
+      res.send('Email already in use.')
+    }
+  }
+
+  RegenIDIfSame(users, myID, 6)
+    users[myID] = {
+    id:myID,
+    email: myEmail,
+    password: myPassword
+  }
+
+  if(!myPassword || !myEmail){
+    res.status(400);
+    res.send('Email and Password is required.');
+  } else{
+
+  res.cookie('user_id', myID);
+  res.redirect('/urls');
+  }
+});
+
+
+app.post("/login", (req, res) =>{
+  let myEmail = req.body.email;
+  let myPassword = req.body.password;
+  for(databaseID in users){
+
+    let databaseEmail = users[databaseID].email
+    let databasePassword = users[databaseID].password
+    console.log("Compare: ",databaseEmail, myEmail)
+    console.log("Compare: ", databasePassword,myPassword)
+    console.log(databaseID)
+    if((databaseEmail == myEmail) &&(databasePassword == myPassword)){
+      console.log('my if statement works')
+      res.cookie('user_id', databaseID);
+      res.redirect('/urls');
+    // } else{
+    //   res.status(400);
+    //   res.send('Incorrect Email or Password')
+    }
+  }
+  res.status(400);
+  res.send('Email and/or Password is incorrect.');
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
