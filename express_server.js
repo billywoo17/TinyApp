49a663be5e426bfeb21342f94140c9eeeefaa528
billywoo17 <billwu_@outlook.com>
2018-06-17 20:15:39 -0700

    fixed lint

--- express_server.js
+++ express_server.js
@@ -13,29 +13,37 @@ const PORT = process.env.PORT || 8080;
 const bodyParser = require("body-parser");
 const bcrypt = require('bcrypt');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.set("view engine", "ejs");
 
 function generateRandomString(length) {
   let text = "";
   const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  for(i = 0; i < length; i++) {
+  for (i = 0; i < length; i++) {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
   return text;
 }
 
-function addHTTP(url, shortURL, myID){
-  if(url.includes(`http`) == true){
-    return urlDatabase[shortURL] = {url: url, id: myID} ;
+function addHTTP(url, shortURL, myID) {
+  if (url.includes(`http`) == true) {
+    return urlDatabase[shortURL] = {
+      url: url,
+      id: myID
+    };
 
   }
   url = "http://" + url;
-  return urlDatabase[shortURL] = {url: url, id: myID};
+  return urlDatabase[shortURL] = {
+    url: url,
+    id: myID
+  };
 }
 
-function RegenIDIfSame(Database, myGenID, value){
-  while(Database.hasOwnProperty(myGenID) === true){
+function RegenIDIfSame(Database, myGenID, value) {
+  while (Database.hasOwnProperty(myGenID) === true) {
     myGenID = generateRandomString(value);
     console.log(`key already exsisted. generating new key ${myGenID}`);
   }
@@ -58,7 +66,7 @@ const users = {
     email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
- "user2RandomID": {
+  "user2RandomID": {
     id: "user2RandomID",
     email: "123123",
     password: "123"
@@ -76,18 +84,18 @@ app.get("/urls.json", (req, res) => {
 app.get("/urls", (req, res) => {
   let templateVars = {
     urls: urlDatabase,
-    username:JSON.stringify(users[req.session.user_id]),
+    username: JSON.stringify(users[req.session.user_id]),
     id: req.session.user_id
-    };
-    console.log(req.session.user_id)
-    console.log(urlDatabase)
+  };
+  console.log(req.session.user_id)
+  console.log(urlDatabase)
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
   let templateVars = {
     urls: urlDatabase,
-    username:JSON.stringify(users[req.session.user_id])
+    username: JSON.stringify(users[req.session.user_id])
   };
   res.render("urls_new", templateVars);
 
@@ -98,20 +106,20 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  if(users[req.session.user_id]){;
+  if (users[req.session.user_id]) {;
     let templateVars = {
       shortURL: req.params.id,
       fullURL: urlDatabase,
-      username:JSON.stringify(users[req.session.user_id].email),
+      username: JSON.stringify(users[req.session.user_id].email),
       id: req.session.user_id
     }
-  res.render("urls_show", templateVars);
-  return;
+    res.render("urls_show", templateVars);
+    return;
   }
   res.redirect("/register");
 });
 
-app.get("/register", (req, res) =>{
+app.get("/register", (req, res) => {
   res.render("urls_register");
 });
 
@@ -120,7 +128,7 @@ app.get("/login", (req, res) => {
 });
 
 app.post("/urls/:id", (req, res) => {
-  if(users[req.session.user_id]){
+  if (users[req.session.user_id]) {
     addHTTP(req.body.longURL, req.params.id, users[req.session.user_id].id);
     res.redirect('/urls/');
     return;
@@ -129,22 +137,22 @@ app.post("/urls/:id", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  if(users[req.session.user_id]){
-      let short = generateRandomString(6);
-      RegenIDIfSame(urlDatabase, short, 6)
-      let newHTTP = addHTTP(req.body.longURL, short ,users[req.session.user_id].id);  urlDatabase[short] = {
+  if (users[req.session.user_id]) {
+    let short = generateRandomString(6);
+    RegenIDIfSame(urlDatabase, short, 6)
+    let newHTTP = addHTTP(req.body.longURL, short, users[req.session.user_id].id);
+    urlDatabase[short] = {
       url: newHTTP.url,
       id: req.session.user_id
     };
-  res.redirect('/urls/');
-  return;
+    res.redirect('/urls/');
+    return;
   }
   res.redirect("/register")
 });
 
-app.post("/urls/:id/delete",(req, res) =>{
-  if( urlDatabase[req.params.id].id === req.session.user_id)
-  {
+app.post("/urls/:id/delete", (req, res) => {
+  if (urlDatabase[req.params.id].id === req.session.user_id) {
     delete urlDatabase[req.params.id];
     res.redirect('/urls');
   }
@@ -152,19 +160,19 @@ app.post("/urls/:id/delete",(req, res) =>{
 });
 
 
-app.post("/logout", (req, res) =>{
+app.post("/logout", (req, res) => {
   req.session = null;
   res.redirect('/urls');
 });
 
-app.post("/register", (req, res) =>{
+app.post("/register", (req, res) => {
   let myID = generateRandomString(6);
   let myEmail = req.body.email;
   let myPassword = req.body.password;
   const hashedPassword = bcrypt.hashSync(myPassword, 10);
 
-  for(databaseID in users){
-    if(users[databaseID].email === myEmail){
+  for (databaseID in users) {
+    if (users[databaseID].email === myEmail) {
       res.status(400);
       res.send('Email already in use.')
       return;
@@ -172,13 +180,13 @@ app.post("/register", (req, res) =>{
   }
 
   RegenIDIfSame(users, myID, 6)
-    users[myID] = {
-    id:myID,
+  users[myID] = {
+    id: myID,
     email: myEmail,
     password: hashedPassword
   }
 
-  if(!myPassword || !myEmail){
+  if (!myPassword || !myEmail) {
     res.status(400);
     res.send('Email and Password is required.');
     return;
@@ -188,17 +196,17 @@ app.post("/register", (req, res) =>{
 
 });
 
-app.post("/login", (req, res) =>{
+app.post("/login", (req, res) => {
   let myEmail = req.body.email;
   let myPassword = req.body.password;
-  for(databaseID in users){
+  for (databaseID in users) {
     let databaseEmail = users[databaseID].email
     let databasePassword = users[databaseID].password
-    if((databaseEmail == myEmail) &&
-      (bcrypt.compareSync(myPassword, databasePassword))){
+    if ((databaseEmail == myEmail) &&
+      (bcrypt.compareSync(myPassword, databasePassword))) {
       req.session.user_id = databaseID;
       res.redirect('/urls');
-    return;
+      return;
     }
   }
   res.status(400);
@@ -208,4 +216,3 @@ app.post("/login", (req, res) =>{
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
